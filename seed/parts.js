a4aea9ff93bab0c2e385d43c970b29baa123c5fd
CinqueaS <cinquea.stafford@gmail.com>
2024-10-15 15:09:33 -0400

    added more parts

--- seed/parts.js
+++ seed/parts.js
@@ -30,16 +30,53 @@ const main = async () => {
             price: 200
         },
         {
+            name: 'Windshield Wiper',
+            partNumber: 64325,
+            brandFrom: Brands[0].name ,
+            price: 6
+        },
+        {
+            name: 'Distributor Cap',
+            partNumber: 67231,
+            brandFrom: Brands[0].name ,
+            price: 56
+        },
+        {
             name: 'Accessory Belt Tensioner',
             partNumber: 89390,
             brandFrom: Brands[1].name ,
             price: 122
         },
         {
+            name: 'Radiator Hose',
+            partNumber: 70081,
+            brandFrom: Brands[1].name ,
+            price: 18
+        },
+        {
+            name: 'Engine Harmonic Balancer',
+            partNumber: 17301,
+            brandFrom: Brands[1].name ,
+            price: 180
+        },
+
+        {
             name: 'Carburetor',
             partNumber: 80457,
             brandFrom: Brands[2].name ,
             price: 456
+        },
+        {
+            name: 'Intake Manifold',
+            partNumber: 19024,
+            brandFrom: Brands[2].name ,
+            price: 1042
+        },
+        {
+            name: 'Fuel Tank and Pump Assembly',
+            partNumber: 91048,
+            brandFrom: Brands[2].name ,
+            price: 800
         },
     ]
     await Brand.insertMany(Brands)
