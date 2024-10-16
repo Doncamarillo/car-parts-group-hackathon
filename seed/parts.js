6d9d1f38e4c6826a7760e2822861d9fc8a64374a
CinqueaS <cinquea.stafford@gmail.com>
2024-10-16 13:21:52 -0400

    seeded

--- seed/parts.js
+++ seed/parts.js
@@ -5,81 +5,84 @@ const Part = require('../models/part')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-    const Brands = [
-        {
-            name: 'Bosch',
-            yearFounded: 1886,
-            region: 'Gerlingen, Baden-Württemberg, Germany',
-        },
-        {
-            name: 'Dayco',
-            yearFounded: 1905,
-            region: 'Birmingham, Michigan, United States'
-        },
-        {
-            name: 'Holley',
-            yearFounded: 1896,
-            region: 'Bowling Green, Kentucky, United States'
-        },
-    ]
+    // const Brands = [
+    //     {
+    //         name: 'bosch',
+    //         yearFounded: 1886,
+    //         region: 'Gerlingen, Baden-Württemberg, Germany'
+    //     }
+    //     {
+    //         name: 'Dayco',
+    //         yearFounded: 1905,
+    //         region: 'Birmingham, Michigan, United States'
+    //     },
+    //     {
+    //         name: 'Holley',
+    //         yearFounded: 1896,
+    //         region: 'Bowling Green, Kentucky, United States'
+    //     },
+    // ]
+    const bosch = await Brand.find({ name: 'bosch' })
+    const dayco = await Brand.find({ name: 'Dayco' })
+    const holley = await Brand.find({name: "Holley"})
     const Parts = [
         {
             name: 'Fuel Pump',
             partNumber: 66800,
-            brandFrom: Brands[0].name ,
+            brandFrom: bosch[0]._id ,
             price: 200
         },
         {
             name: 'Windshield Wiper',
             partNumber: 64325,
-            brandFrom: Brands[0].name ,
+            brandFrom: bosch[0]._id ,
             price: 6
         },
         {
             name: 'Distributor Cap',
             partNumber: 67231,
-            brandFrom: Brands[0].name ,
+            brandFrom: bosch[0]._id ,
             price: 56
         },
         {
             name: 'Accessory Belt Tensioner',
             partNumber: 89390,
-            brandFrom: Brands[1].name ,
+            brandFrom: dayco[0]._id ,
             price: 122
         },
         {
             name: 'Radiator Hose',
             partNumber: 70081,
-            brandFrom: Brands[1].name ,
+            brandFrom: dayco[0]._id ,
             price: 18
         },
         {
             name: 'Engine Harmonic Balancer',
             partNumber: 17301,
-            brandFrom: Brands[1].name ,
+            brandFrom: dayco[0]._id ,
             price: 180
         },
 
         {
             name: 'Carburetor',
             partNumber: 80457,
-            brandFrom: Brands[2].name ,
+            brandFrom: holley[0]._id ,
             price: 456
         },
         {
             name: 'Intake Manifold',
             partNumber: 19024,
-            brandFrom: Brands[2].name ,
+            brandFrom: holley[0]._id ,
             price: 1042
         },
         {
             name: 'Fuel Tank and Pump Assembly',
             partNumber: 91048,
-            brandFrom: Brands[2].name ,
+            brandFrom: holley[0]._id ,
             price: 800
         },
     ]
-    await Brand.insertMany(Brands)
+    //await Brand.insertMany(Brands)
     await Part.insertMany(Parts)
     console.log("Created brands with car parts")
 }
