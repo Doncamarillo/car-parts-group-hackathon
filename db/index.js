54d5c477ef9d8fdd877c02a4619b6ddb92851e90
CinqueaS <cinquea.stafford@gmail.com>
2024-10-16 16:53:21 -0400

    finished

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb+srv://doncamarillo:doncamarillo@student-cluster.qqopd.mongodb.net/parts?retryWrites=true&w=majority&appName=student-cluster')
+    .connect('mongodb+srv://cinqueastafford:Quiche214C@student-cluster.qk4nd.mongodb.net/carPartsDatabase?retryWrites=true&w=majority&appName=student-cluster')
     .then(() => {
         console.log('Successfully connected to MongoDB.')
     })
